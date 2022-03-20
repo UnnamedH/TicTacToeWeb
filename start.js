21b1ac14e32a4c716ac6b9814fc223334dda2cac
Hrant Elmayan <hrantelmayan@gmail.com>
2022-03-20 11:21:46 +0200

    Fixing file names and clean up

--- start.js
+++
@@ -1,8 +0,0 @@
-let path = window.location;
-let scoreP1 = 0;
-let scoreP2 = 0;
-
-localStorage.setItem('scoreP1', scoreP1);
-localStorage.setItem('scoreP2', scoreP2);
-
-path.replace('index.html');
